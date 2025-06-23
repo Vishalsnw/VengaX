2f8133af6931f84ece27f40f1158eb4e7615db14
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-06-23 12:06:48 +0530

    Create SplashActivity.java

---
+++ app/src/main/java/com/vengax/app/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.vengax.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    private static final int SPLASH_TIME_OUT = 2500; // 2.5 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            Intent i = new Intent(SplashActivity.this, WebViewActivity.class);
+            startActivity(i);
+            finish();
+        }, SPLASH_TIME_OUT);
+    }
+}
